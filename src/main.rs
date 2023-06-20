988ebd1ce805bcaff44de255f73bef1693942953
tu10ng <2059734099@qq.com>
2023-06-21 02:07:57 +0800

    (feature) argparse

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,18 @@
+use clap::Parser;
 use std::net::{IpAddr, SocketAddr, ToSocketAddrs};
 
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli
+{
+    /// dns name or ip address
+    destination: String,
+}
+
+
 fn main() {
-    let addr = ("www.example.com", 80).to_socket_addrs().unwrap();
+    let args = Cli::parse();
+    
+    let addr = (args.destination, 80).to_socket_addrs().unwrap().nth(0).unwrap().ip();
     println!("{:?}", addr);
 }
