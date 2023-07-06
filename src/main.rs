92d51e440ae2df3ca87631a5d14fc54a2aac19a7
onebitbool <onebitbool@gmail.com>
2023-07-07 00:30:28 +0800

    Change: broadcast statistics

--- src/main.rs
+++ src/main.rs
@@ -212,13 +212,21 @@ fn run(config: Config) {
     // when \C-c is pressed
     println!();
     println!("--- {} rping statistics ---", config.destination);
-    println!(
-        "{} packets transmitted, {} received, {}% packet loss, time {}ms",
-        sequence,
-        stat_received,
-        (sequence - stat_received) / sequence,
-        Instant::now().duration_since(time_init).as_millis()
-    );
+    if config.broadcast {
+        println!(
+            "{} packets transmitted, time {}ms",
+            sequence,
+            Instant::now().duration_since(time_init).as_millis()
+        );
+    } else {
+        println!(
+            "{} packets transmitted, {} received, {}% packet loss, time {}ms",
+            sequence,
+            stat_received,
+            (sequence - stat_received) / sequence,
+            Instant::now().duration_since(time_init).as_millis()
+        );
+    }
 }
 
 fn ping(
